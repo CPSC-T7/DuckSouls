0e2b150efca631fbab05e2ddfb20350cf866ce4a
Matthew Allwright <matt.a.4890@gmail.com>
2018-03-03 15:35:40 -0700

    Formatting as making UML

--- DuckSoulsText-Workspace/DuckSouls_Text/src/tiles/Floor.java
+++ DuckSoulsText-Workspace/DuckSouls_Text/src/tiles/Floor.java
@@ -1,9 +1,32 @@
 package tiles;
 
+/**
+ * This class represents all of the floor tiles for DuckSouls.
+ * 
+ * @author Matthew Allwright
+ * @version 1.1
+ */
 public class Floor extends Tile {
 	
+	/*
+	 * 
+	 * INSTANCE VARIABLES
+	 * 
+	 */
+	
+	private static final String stringRepr = "   ";
+	
+	/*
+	 * 
+	 * CONSTRUCTORS
+	 * 
+	 */
+	
+	/**
+	 * Creates a floor tile.
+	 */
 	public Floor() {
-		super("   ", true);
+		super(stringRepr, true);
 	}
 
 }
