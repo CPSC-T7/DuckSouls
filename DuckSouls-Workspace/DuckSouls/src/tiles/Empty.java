fab3d1794fc5f9d689882a0e760d696a746c6da3
Matthew Allwright <matt.a.4890@gmail.com>
2018-03-16 11:04:29 -0600

    Updated some tile thingies (Floor text)

---
+++ DuckSouls-Workspace/DuckSouls/src/tiles/Empty.java
@@ -0,0 +1,36 @@
+package tiles;
+
+/**
+ * This class represents all of the path tiles for DuckSouls.
+ * 
+ * @author Matthew Allwright
+ * @version 1.1
+ */
+public class Empty extends Tile {
+	
+	/*
+	 * 
+	 * INSTANCE VARIABLES
+	 * 
+	 */
+	
+	private static final String stringRepr = "   ";
+	private static final String	pathToImage	= "/Sprites/Tiles/Sewer/Floor.png";
+	
+	/*
+	 * 
+	 * CONSTRUCTORS
+	 * 
+	 */
+	
+	/**
+	 * Creates an empty tile.
+	 * 
+	 * @param isGUI
+	 *            Whether this is a GUI tile or not.
+	 */
+	public Empty() {
+		super(stringRepr, true, pathToImage);
+	}
+
+}
