de569aa56d30d2cfd892e0db5c26468b7821a2ca
Matthew Allwright <matt.a.4890@gmail.com>
2018-04-03 17:14:32 -0600

    Explainations

--- DuckSouls-Workspace/DuckSouls-Remaster/src/ui/RoomDrawer.java
+++ DuckSouls-Workspace/DuckSouls-Remaster/src/ui/RoomDrawer.java
@@ -1,15 +1,57 @@
 package ui;
 
-import java.awt.Image;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 
 import world.Room;
 
 public class RoomDrawer {
 	
+	/*
+	 * 
+	 * CONSTANTS
+	 * 
+	 */
+
+	private static final int TILES = 0;
+	private static final int ITEMS = 1;
+	private static final int ENTITIES = 2;
+	
+	/*
+	 * 
+	 * VARIABLES
+	 * 
+	 */
+	
+	private static GraphicsContext gc;
+	
+	/*
+	 * 
+	 * METHODS
+	 * 
+	 */
+	
+	public static void setGraphicsContext(GraphicsContext gc) {
+		RoomDrawer.gc = gc;
+	}
+	
 	public static void drawRoom(Room room, boolean isGUI) {
 		
 		if (isGUI) {
 			
+			Image[][][] sprites = room.getAllImageSprites();
+			
+			for (int x = 0; x < sprites.length; x++) {
+				for (int y = 0; y < sprites[0].length; y++) {
+					
+					/*
+					 * sprites[x][y][TILES] --> tile @ (x, y)
+					 * sprites[x][y][ITEMS] --> item @ (x, y)
+					 * sprites[x][y][ENTITIES] --> entity @ (x, y)
+					 */
+					
+				}
+			}
 			
 		} else {
 			
