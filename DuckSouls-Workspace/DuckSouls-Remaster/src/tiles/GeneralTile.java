b8d51597904a3ee0e77be30b7aaf84d88821d013
Matthew Allwright <matt.a.4890@gmail.com>
2018-04-04 19:32:31 -0600

    Adding Water

--- DuckSouls-Workspace/DuckSouls-Remaster/src/tiles/GeneralTile.java
+++ DuckSouls-Workspace/DuckSouls-Remaster/src/tiles/GeneralTile.java
@@ -10,10 +10,21 @@ public enum GeneralTile implements Tile {
 	 * 
 	 */
 	
-	// EMPTY("XXX", "Empty.png", false),
+	EMPTY("XXX", "Empty.png", false),
 	FLOOR("   ", "Floor.png", true),
 	PATH(" . ", "Floor.png", true),
-	STAIRS("/=/", "Stairs.png", true);
+	STAIRS("/=/", "Stairs.png", true),
+	
+	// Water
+
+	WATER_T("###", "Water-Top.png", false),
+	WATER_B("###", "Water-Bottom.png", false),
+	WATER_L(" ##", "Water-Left.png", false),
+	WATER_R("## ", "Water-Right.png", false),
+	WATER_TL(" ##", "Water-Top-Left.png", false),
+	WATER_TR("## ", "Water-Top-Right.png", false),
+	WATER_BL(" ##", "Water-Bottom-Left.png", false),
+	WATER_BR("## ", "Water-Bottom-Right.png", false);
 	
 	/*
 	 * 
